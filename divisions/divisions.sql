d17c252cc4938cd17bde40562c0ad054a73f80ff
sanjib3 <sanjib@shurjomukhi.com.bd>
2024-01-30 18:10:41 +0600

    Added geo code for all divisions according to bbs.gov.bd

--- divisions/divisions.sql
+++ divisions/divisions.sql
@@ -32,22 +32,23 @@ CREATE TABLE `divisions` (
   `id` int(1) NOT NULL,
   `name` varchar(25) NOT NULL,
   `bn_name` varchar(25) NOT NULL,
-  `url` varchar(50) NOT NULL
+  `url` varchar(50) NOT NULL,
+  `geo_code` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `divisions`
 --
 
-INSERT INTO `divisions` (`id`, `name`, `bn_name`, `url`) VALUES
-(1, 'Chattagram', 'চট্টগ্রাম', 'www.chittagongdiv.gov.bd'),
-(2, 'Rajshahi', 'রাজশাহী', 'www.rajshahidiv.gov.bd'),
-(3, 'Khulna', 'খুলনা', 'www.khulnadiv.gov.bd'),
-(4, 'Barisal', 'বরিশাল', 'www.barisaldiv.gov.bd'),
-(5, 'Sylhet', 'সিলেট', 'www.sylhetdiv.gov.bd'),
-(6, 'Dhaka', 'ঢাকা', 'www.dhakadiv.gov.bd'),
-(7, 'Rangpur', 'রংপুর', 'www.rangpurdiv.gov.bd'),
-(8, 'Mymensingh', 'ময়মনসিংহ', 'www.mymensinghdiv.gov.bd');
+INSERT INTO `divisions` (`id`, `name`, `bn_name`, `url`, `geo_code`) VALUES
+(1, 'Chattagram', 'চট্টগ্রাম', 'www.chittagongdiv.gov.bd', 20),
+(2, 'Rajshahi', 'রাজশাহী', 'www.rajshahidiv.gov.bd', 50),
+(3, 'Khulna', 'খুলনা', 'www.khulnadiv.gov.bd', 40),
+(4, 'Barisal', 'বরিশাল', 'www.barisaldiv.gov.bd', 10),
+(5, 'Sylhet', 'সিলেট', 'www.sylhetdiv.gov.bd', 60),
+(6, 'Dhaka', 'ঢাকা', 'www.dhakadiv.gov.bd', 30),
+(7, 'Rangpur', 'রংপুর', 'www.rangpurdiv.gov.bd', 55),
+(8, 'Mymensingh', 'ময়মনসিংহ', 'www.mymensinghdiv.gov.bd', 45);
 
 --
 -- Indexes for dumped tables
